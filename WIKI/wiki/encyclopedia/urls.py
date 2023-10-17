244cdda598384103b053a523805449be23e6fdbb
habiburahmanrulialif <habibalif865@gmail.com>
2023-10-18 05:36:22 +0700

    adding entry page and new page

--- WIKI/wiki/encyclopedia/urls.py
+++ WIKI/wiki/encyclopedia/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("add_page", views.add_page, name="add_page"),
+    path("<str:entry>", views.entry, name="entry")
 ]
--- WIKI/wiki/encyclopedia/views.py
+++ WIKI/wiki/encyclopedia/views.py
@@ -8,3 +8,12 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+def entry(request, entry):
+    return render(request, "encyclopedia/entry-page.html", {
+        "title": entry
+    })
+
+
+def add_page(request):
+    return render(request, "encyclopedia/add-page.html")
