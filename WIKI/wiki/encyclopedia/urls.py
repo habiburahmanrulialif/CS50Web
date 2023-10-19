548d2dcc3f459de67abfbc5e613d6b9bf95e0cba
habiburahmanrulialif <habibalif865@gmail.com>
2023-10-19 10:47:04 +0700

    adding error page and search function

--- WIKI/wiki/encyclopedia/urls.py
+++ WIKI/wiki/encyclopedia/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("new_page", views.new_page, name="add_page"),
+    path("search", views.search, name="search"),
     path("<str:entry>", views.entry, name="entry")
 ]
--- WIKI/wiki/encyclopedia/views.py
+++ WIKI/wiki/encyclopedia/views.py
@@ -4,6 +4,9 @@ from . import util
 
 import markdown2
 
+def all_upper(my_list):
+            return [x.upper() for x in my_list]
+
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
@@ -27,4 +30,25 @@ def new_page(request):
 
 
 def search(request):
-    find = entry(request)
+    querry = request.GET.get('q')
+    querry = querry.upper()
+    if util.get_entry(querry) is None:
+        entries = util.list_entries()
+        entries = all_upper(entries)
+        entry = [i for i in entries if querry in i]
+        if entry:
+            return render(request, "encyclopedia/index.html", {
+        "entries": entry
+            })
+        
+        '''
+        Should return 404 page
+        '''
+        return render(request, "encyclopedia/404.html")
+    
+    
+    text = markdown2.Markdown()
+    return render(request, "encyclopedia/entry-page.html", {
+        "title": querry,
+        "entries": text.convert(util.get_entry(querry))
+    })
