b17c0b4493c455bbbb6c4e3b37c2b6704c5076e3
habiburahmanrulialif <habibalif865@gmail.com>
2023-12-22 16:58:14 +0700

    finishing model for network

--- NETWORK/project4/network/admin.py
+++ NETWORK/project4/network/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import User, Post, Follow
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    readonly_fields = ("id", "like_count")
+
+class FollowAdmin(admin.ModelAdmin):
+    readonly_fields = ("follower_count", )
+
+admin.site.register(User)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Follow, FollowAdmin)
--- NETWORK/project4/network/models.py
+++ NETWORK/project4/network/models.py
@@ -1,19 +1,37 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
+from django_resized import ResizedImageField
+import os
+from uuid import uuid4
 
 class User(AbstractUser):
     pass
 
 
-class Post:
+def path_and_rename(instance, filename):
+    upload_to = 'network/static/network/image'
+    ext = filename.split('.')[-1]
+    # get filename
+    if instance.pk:
+        filename = '{}.{}'.format(instance.pk, ext)
+    else:
+        # set filename as random string
+        filename = '{}.{}'.format(uuid4().hex, ext)
+    # return the whole path to the file
+    return os.path.join(upload_to, filename)
+
+
+class Post(models.Model):
     post_owner = models.ForeignKey("User", on_delete=models.CASCADE, related_name = "poster")
-    post_image = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=None)
+    post_image = ResizedImageField(size=[1920, 1080], upload_to=path_and_rename, null=True, blank=True)
     post_text = models.TextField()
     post_time = models.DateTimeField(auto_now_add=True)
-    post_like = models.ManyToManyField("User", related_name="post like")
-    post_like_count = models.IntegerField()
+    post_like = models.ManyToManyField("User", related_name="post_like", blank=True)
 
+    def like_count(self):
+        return self.post_like.all().count()
+
+    
     def serialize(self):
         return {
             "post_id": self.id,
@@ -21,21 +39,26 @@ class Post:
             "post_image": self.post_image,
             "post_text" : self.post_text,
             "post_time" : self.post_time.strftime("%b %d %Y, %I:%M %p"),
-            "post_like" : [user.username for user in self.post_like],
-            "post_like_count" : self.post_like_count
+            "post_like" : [user.username for user in self.post_like.all()],
+            "post_like_count" : self.like_count()
         }
+    
+    def __str__(self):
+        return f'{self.post_owner} - {self.id}'
+    
 
 
-class Follow:
-    account = models.ForeignKey("User", on_delete=models.CASCADE, related_name="follow account")
-    follower = models.ManyToManyField("User", related_name="follower")
-    follower_count = models.IntegerField()
-    following = models.ManyToManyField("User", related_name="following")
+class Follow(models.Model):
+    account = models.ForeignKey("User", on_delete=models.CASCADE, related_name="follow_account")
+    follower = models.ManyToManyField("User", related_name="follower", blank=True)
+    following = models.ManyToManyField("User", related_name="following", blank=True)
+
+    def follower_count(self):
+        return self.follower.all().count()
 
     def serialize(self):
         return {
             "account" : self.account,
-            "follower" : [user.username for user in self.follower],
-            "follower_count" : self.follower_count,
-            "following" : [user.username for user in self.following]
+            "follower" : [user.username for user in self.follower.all()],
+            "following" : [user.username for user in self.following.all()]
         }
