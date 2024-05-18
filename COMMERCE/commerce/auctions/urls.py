b83aecd039ecf5e1df689561c16f7ef4de60da87
habiburahmanrulialif <habibalif865@gmail.com>
2024-05-18 14:29:35 +0700

    testing static

--- COMMERCE/commerce/auctions/urls.py
+++ COMMERCE/commerce/auctions/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
-from django.conf.urls.static import static
-from django.conf import settings
 from . import views
 
 urlpatterns = [
@@ -16,5 +14,3 @@ urlpatterns = [
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register")
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
